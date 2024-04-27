58ef0d4147f571fb77cb7f36178032599c68770b
KID <50666207+KID421@users.noreply.github.com>
2024-04-27 12:00:25 +0800

    Update 完成分數系統

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField, Header("時間文字")]
+    private TMP_Text textTime;
+    [SerializeField, Header("倒數時間"), Range(10, 100)]
+    private float timeTotal = 60;
+
+    // 更新事件：一秒執行約 60 次
+    private void Update()
+    {
+        CountDown();
+    }
+
+    private void CountDown()
+    {
+        // 時間 扣除 每幀的時間 (約為 1/60 秒)
+        timeTotal -= Time.deltaTime;
+        // 更新 時間文字
+        textTime.text = $"TIME - {timeTotal.ToString("F2")}";
+    }
+}
